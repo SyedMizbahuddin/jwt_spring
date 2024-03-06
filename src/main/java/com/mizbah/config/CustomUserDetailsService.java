641e8ddb2dd47fa949d31f4e2304cdd436dfc73f
Syed Mizbahuddin <syed@nworx.ai>
2024-03-07 02:29:41 +0530

    ADDED security config

---
+++ src/main/java/com/mizbah/config/CustomUserDetailsService.java
@@ -0,0 +1,34 @@
+package com.mizbah.config;
+
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.mizbah.entity.Employee;
+import com.mizbah.repository.EmployeeRepository;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+	EmployeeRepository employeeRepository;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		Employee employee = employeeRepository.findByEmail(username);
+		if (employee == null) {
+			throw new UsernameNotFoundException("User with email not found: " + username);
+		}
+
+		UserDetails userDetails = User.builder().username(employee.getEmail()).password(employee.getPassword())
+				.roles(employee.getRole().name()).build();
+		return userDetails;
+	}
+
+}
