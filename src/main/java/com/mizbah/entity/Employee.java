652f95a4936304fe4c05fff294960d4026ff6304
Syed Mizbahuddin <syed@nworx.ai>
2024-03-07 02:28:59 +0530

    ADDED entities

---
+++ src/main/java/com/mizbah/entity/Employee.java
@@ -0,0 +1,42 @@
+package com.mizbah.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "employee")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Employee {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private int id;
+
+	@Column(name = "name")
+	private String name;
+
+	@Column(name = "email")
+	private String email;
+
+	@Column(name = "password")
+	private String password;
+
+	@Enumerated(EnumType.STRING)
+	@Column(name = "role")
+	private RoleType role;
+
+}
---
+++ src/main/java/com/mizbah/entity/RoleType.java
@@ -0,0 +1,5 @@
+package com.mizbah.entity;
+
+public enum RoleType {
+	ADMIN
+}
